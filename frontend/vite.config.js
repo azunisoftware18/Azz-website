bf18b7ffd5f58a70cc2c98a190f3d3a961ea2f3a
azunisoftware18 <azunisoftware18@gmail.com>
2026-09-01 13:50:23 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'jue6j7tmf90h6t8ehdrk6isw.147.93.20.127.sslip.io'
+    ]
+  }
 })
